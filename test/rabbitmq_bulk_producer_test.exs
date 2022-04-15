c939dc6b03a8b7f0da879c9a26e58a76e888487d
Paolo Laurenti <laurentipaolo@gmail.com>
2022-04-15 14:35:53 +0200

    fix code format and credo issues

--- test/rabbitmq_bulk_producer_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RabbitmqBulkProducerTest do
-  use ExUnit.Case
-  doctest RabbitmqBulkProducer
-
-  test "greets the world" do
-    assert RabbitmqBulkProducer.hello() == :world
-  end
-end
